a351691de4e9b390828ccb29e93d241e0d1004b4
sokuo <sokuo1748@gmail.com>
2025-05-28 10:06:49 +0800

    add aboutMePage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,29 @@
 import AboutMe from "@/component/about-me/about-me";
+import { useAppContext } from "@/context/appContext";
+import { useCallback } from "react";
 
 export default function HomePage() {
+    const { activeItem } = useAppContext();
+
+    console.log('activeItem', activeItem)
+    const page = useCallback(() => {
+        switch (activeItem) {
+            case 'ABOUT ME':
+                return <AboutMe />
+            case 'RESUME':
+                return (<>
+                    <div>123</div>
+                </>)
+            case 'PROJECTS':
+                return (<>
+                    <div>456</div>
+                </>)
+        }
+    }, [activeItem])
     return (
         <>
-            <div className={`homePageMain`}>        
-                <AboutMe />
+            <div className={`homePageMain`}>
+                {page()}
             </div>
         </>
     );
